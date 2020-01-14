1ddbfa6c1d3f8e6c16c8c5ebe79febf58a9444e4
Kevin Wang <bird831205@hotmail.com>
2020-01-14 08:58:24 -0500

    palindrome counter

--- projects/assessments/Maven.Assessment1.2/src/main/java/com/github/curriculeon/assessment1/part5/Palindrome.java
+++ projects/assessments/Maven.Assessment1.2/src/main/java/com/github/curriculeon/assessment1/part5/Palindrome.java
@@ -4,8 +4,18 @@ import com.github.curriculeon.assessment1.part1.BasicStringUtils;
 
 public class Palindrome {
 
+
+
     public Integer countPalindromes(String input){
-        return null;
+        Integer count = 0;
+        for(int wordLength=1; wordLength<=input.length(); wordLength++){
+            for(int index=0; index<input.length()-wordLength+1;index++){
+                if( isPalindrome( input.substring(index,index+wordLength) ) ){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     private boolean isPalindrome(String input){
