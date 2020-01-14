3be0071c9a445ef443c8f259e1e0b95de505cc4d
Kevin Wang <bird831205@hotmail.com>
2020-01-14 10:17:51 -0500

    inventory

--- projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/collections/Inventory.java
+++ projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/collections/Inventory.java
@@ -1,5 +1,6 @@
 package com.github.curriculeon.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,29 +10,32 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
-    public Inventory(List<String> strings) {
 
+    List<String> strings;
+
+    public Inventory(List<String> strings) {
+        this.strings = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        this(new ArrayList<>());
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        strings.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        strings.remove(item);
     }
 
     /**
@@ -39,6 +43,12 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        Integer count = 0;
+        for(String s : strings){
+            if(s.equals(item)){
+                count++;
+            }
+        }
+        return count;
     }
 }
