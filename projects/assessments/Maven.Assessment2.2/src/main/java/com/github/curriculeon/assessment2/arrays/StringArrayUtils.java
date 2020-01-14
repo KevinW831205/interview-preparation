e7e3f78a02f94b6faa7654d25794bc32109d2f2a
Kevin Wang <bird831205@hotmail.com>
2020-01-14 10:13:59 -0500

    string array utils

--- projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/arrays/StringArrayUtils.java
+++ projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/arrays/StringArrayUtils.java
@@ -6,21 +6,33 @@ package com.github.curriculeon.assessment2.arrays;
 public class StringArrayUtils {
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
-     * @param endingIndex - ending index of array
+     * @param startingIndex    - starting index of array to be spliced
+     * @param endingIndex      - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
-        return null;
+        if (startingIndex < 0 || endingIndex < 0) {
+            throw new IllegalArgumentException();
+        }
+        String[] result = new String[endingIndex - startingIndex];
+        for (int i = startingIndex; i < endingIndex; i++) {
+            result[i - startingIndex] = arrayToBeSpliced[i];
+        }
+        return result;
     }
 
 
     /**
      * @param arrayToBeSpliced - array to be evaluated
-     * @param startingIndex - starting index of array to be spliced
+     * @param startingIndex    - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+        if(startingIndex>arrayToBeSpliced.length){
+            throw new IllegalArgumentException();
+        } else if( startingIndex<0){
+            throw new IndexOutOfBoundsException();
+        }
+        return getSubArray(arrayToBeSpliced, startingIndex, arrayToBeSpliced.length);
     }
 }
