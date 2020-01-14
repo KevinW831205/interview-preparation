feea148d5aa5dccd7fdc9e68cd4cee1721dc820d
Kevin Wang <bird831205@hotmail.com>
2020-01-14 10:57:21 -0500

    StringUtils

--- projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/fundamentals/StringUtils.java
+++ projects/assessments/Maven.Assessment2.2/src/main/java/com/github/curriculeon/assessment2/fundamentals/StringUtils.java
@@ -10,7 +10,12 @@ public class StringUtils {
      * @return `stringToBePadded` flushed right by left-padding
      */
     public static String padLeft(String stringToBePadded, int amountOfPadding) {
-        return null;
+        String result = "";
+        for(int i= stringToBePadded.length(); i<amountOfPadding;i++){
+            result += " ";
+        }
+        result += stringToBePadded;
+        return result;
     }
 
     /**
@@ -19,7 +24,13 @@ public class StringUtils {
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
-        return null;
+        String result = "";
+        result += stringToBePadded;
+        for(int i= stringToBePadded.length(); i<amountOfPadding;i++){
+            result += " ";
+        }
+        return result;
+
     }
 
     /**
@@ -28,7 +39,11 @@ public class StringUtils {
      * @return the string repeated and concatenated `n` times
      */
     public static String repeatString(String stringToBeRepeated, int numberOfTimeToRepeat) {
-        return null;
+        String result = "";
+        for(int i=0; i<numberOfTimeToRepeat; i++){
+            result += stringToBeRepeated;
+        }
+        return result;
     }
 
     /**
@@ -36,7 +51,8 @@ public class StringUtils {
      * @return - true if string only contains alpha characters
      */
     public static Boolean isAlphaString(String string) {
-        return null;
+        String regex = "^[A-Za-z\\s]+$";
+        return string.matches(regex);
     }
 
     /**
@@ -44,7 +60,8 @@ public class StringUtils {
      * @return - true if string only contains numeric characters
      */
     public static Boolean isNumericString(String string) {
-        return null;
+        String regex = "^[0-9]+$";
+        return string.matches(regex);
     }
 
     /**
@@ -52,6 +69,7 @@ public class StringUtils {
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        return null;
+        String regex = "^[`~!@#$%^&*()_+\\-\\\\=;:'\"?/.,<>]+$";
+        return string.matches(regex);
     }
 }
