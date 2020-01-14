0e28cb0034818450b6e5450c422a248368afdd89
Kevin Wang <bird831205@hotmail.com>
2020-01-14 08:42:39 -0500

    is palindrome

--- projects/assessments/Maven.Assessment1.2/src/main/java/com/github/curriculeon/assessment1/part5/Palindrome.java
+++ projects/assessments/Maven.Assessment1.2/src/main/java/com/github/curriculeon/assessment1/part5/Palindrome.java
@@ -1,8 +1,15 @@
 package com.github.curriculeon.assessment1.part5;
 
+import com.github.curriculeon.assessment1.part1.BasicStringUtils;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
         return null;
     }
+
+    private boolean isPalindrome(String input){
+        return input.equals(BasicStringUtils.reverse(input));
+    }
+
 }
